7940a07072937dc8189ee671ef346267153fecab
David Kwast <david@kwast.me>
2018-05-09 17:40:31 -0300

    wemos_D1_LED/blink.py improvement

--- wemos_D1_LED/blink.py
+++ wemos_D1_LED/blink.py
@@ -3,15 +3,20 @@ from machine import Pin
 
 led_pin = Pin(14, Pin.OUT)
 
-def start():
+def start(times=5):
     """
     Blinks LED on Wemos D1 boards
     Pin: 14
     """
     print('Blinking LED on Pin 14 (please press "CTRL + C" to stop)')
     
-    while(True):
+    for c in range(times):
         led_pin.on()
         time.sleep(1)
         led_pin.off()
         time.sleep(1)
+    
+    print('Blinked {} times'.format(times))
+
+if __name__ == '__main__':
+    start()
